68062b6fa53dd4e3b9da03c673dee19254436239
Benjamin Estela <benjaminestela@nevada.unr.edu>
2021-12-05 20:43:08 -0800

    Use Calculations class to print both answers in day02

--- day02/main.py
+++ day02/main.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
 
-aim = 0
-answer = 0
-position = 0
-depth = 0
+class Calcuations:
+    aim = 0
+    position = 0
+    depth = 0
 
-file = open('input.txt', 'r').read().splitlines()
+p1 = Calcuations()
+p2 = Calcuations()
 
-for line in file:
+for line in open('input.txt', 'r').read().splitlines():
     if(line.find('down') != -1):
-        aim += int(line.split('down ')[1])
+        p1.depth += int(line.split('down ')[1])
+        p2.aim += int(line.split('down ')[1])
     elif(line.find('up') != -1):
-        aim -= int(line.split('up ')[1])
+        p1.depth -= int(line.split('up ')[1])
+        p2.aim -= int(line.split('up ')[1])
     elif(line.find('forward') != -1):
-        position += int(line.split('forward ')[1])
-        depth += aim * int(line.split('forward ')[1])
+        p1.position += int(line.split('forward ')[1])
+        p2.position += int(line.split('forward ')[1])
+        p2.depth += p2.aim * int(line.split('forward ')[1])
 
-answer = position * depth
-print('Answer:', answer)
+print('Part 1:', p1.position * p1.depth)
+print('Part 2:', p2.position * p2.depth)
